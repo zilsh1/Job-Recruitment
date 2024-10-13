aa7b77ea4754bde5058e812d6ed1d204e298c20e
zilsh1 <159803416+zilsh1@users.noreply.github.com>
2024-10-13 13:01:19 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export const content = [
+  "./src/**/*.{js,jsx,ts,tsx}",
+  
+  './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
